78ce6e274cea40031902a598f851e0ad10a8d7b7
rstrandberg <rstrandberg@users.noreply.github.com>
2015-12-16 13:45:26 +0100

    Initial implementation of Client

--- src/com/example/gc15/client/Client.java
+++ src/com/example/gc15/client/Client.java
@@ -10,23 +10,61 @@ public class Client {
 	private Socket socket;
 	private BufferedReader in;
 	private PrintWriter out;
-	private Thread listenThread;
+	private Thread listeningThread;
 	
 	public void setName(String name){
 		this.name = name;
 	}
-	
-	public boolean connect(String host, int port){
-		//TODO implement
-		return false;
+	public String getName(){
+		return this.name;
 	}
 	
-	public void disconnect(){
-		//TODO implement
+	public boolean connect(String host, int port){
+		try{
+			socket = new Socket(host, port);
+			out = new PrintWriter(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));		
+		}catch (IOException e){
+			System.out.println(e+" : connect");
+			return false;
+		}
+		connected = true;
+		listeningThread = new Thread(new ListeningThread());
+		listeningThread.start();
+		return true;
+	}
+	
+	public void disconnect() {
+		try{
+			connected = false;
+			in = null;
+			out = null;
+			socket.close();
+		}catch (IOException e){
+			System.out.println(e+" : disconnect");
+		}
 	}
 	
 	public void send(String msg){
-		//TODO implement
+		out.println(msg);
+		out.flush();
+	}
+	
+	private class ListeningThread implements Runnable{
+		@Override
+		public void run() {
+			String msg;
+			try{
+				while(connected){
+					msg = in.readLine();
+					
+					//TODO add message handling logic
+					
+				}
+			}catch (IOException e){
+				System.out.println(e+" : ListeningThread");
+			}		
+		}
 	}
 
 }
