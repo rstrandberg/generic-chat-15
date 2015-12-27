6a8de6cc369b5cd1c05ca99249e8299ebae1d029
Bengt <Bengt@155.4.23.38>
2015-12-27 17:28:04 +0100

    Added file consoleListener to check inputs from console and serverApplication that takes port as input and then starts the server and consoleListerner. Also adds some changes to Clienthandler and Server.

--- src/com/example/gc15/server/ClientHandler.java
+++ src/com/example/gc15/server/ClientHandler.java
@@ -3,20 +3,65 @@ package com.example.gc15.server;
 import java.io.*;
 import java.net.*;
 
-public class ClientHandler {
+import com.example.gc15.server.ClientHandler;
+import com.example.main.Link;
+
+public class ClientHandler implements Runnable{
 	
 	private String userName;
 	private Socket clientSocket;
 	private BufferedReader in;
 	private PrintWriter out;
 	private Thread listeningThread;
+	private ClientHandler next;
 	
-	public ClientHandler(Socket clientSocket, String userName){
-		//TODO implement
+	
+	public ClientHandler(Socket clientSocket){
+		this.clientSocket = clientSocket;
+		try {
+			in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+	        out = new PrintWriter(this.clientSocket.getOutputStream(), true);
+		}
+		catch(IOException ioEx) {
+			ioEx.printStackTrace();
+		}
 	}
 	
 	public void send(String msg){
-		//TODO implement
+		out.println(msg);
+	}
+	
+	public void display() {
+		System.out.print("This thread handles communication with " + this.userName);
+	}
+	
+	public ClientHandler getNext() {
+		return this.next;
+	}
+	
+	public void setNext(ClientHandler next) {
+		this.next = next;
+	}
+	
+	public void run() {
+		
+		String received;
+		try {
+			do {		
+				received = in.readLine();
+				out.println("ECHO: " + received);
+				
+			} while (!received.equals("QUIT"));
+			
+			if(this.clientSocket!=null) {
+				System.out.println("Closing down connection...");
+				this.clientSocket.close();
+			}
+		}
+		catch(IOException ioEx) {
+			System.out.println("Unable to disconnect!");
+		}
+	
 	}
 	
 }
--- src/com/example/gc15/server/Server.java
+++ src/com/example/gc15/server/Server.java
@@ -1,18 +1,29 @@
 package com.example.gc15.server;
 
 import java.net.*;
+import java.io.IOException;
+import java.net.*;
 
-public class Server {
+public class Server implements Runnable {
 	
-	private int port;
-	private boolean started;
-	private ServerSocket serverSocket;
+	private static int PORT;
+	
+	private LinkList clientList;
+	private volatile boolean running;
+	private static ServerSocket serverSocket;
 	private Thread serverThread;
 
 	public Server(int port){
-		//TODO implement
+		this.PORT = port;
+		this.clientList = new LinkList();	
 	}
 	
+	public void initiateShutdown() {
+		this.running = false;
+		
+	}
+	
+	/*
 	public void setPort(int port){
 		//TODO implement
 	}
@@ -35,6 +46,35 @@ public class Server {
 	
 	public void removeClient(String name){
 		//TODO implement
+	}
+	*/
+	
+	public void run() {
+		try {
+			serverSocket = new ServerSocket(PORT);
+			running = true;
+		}
+		catch (IOException ioEx) {
+			System.out.println("\nUnable to set up port!");
+			System.exit(1);
+		}
+		
+		do {
+			
+			try {
+				Socket client = serverSocket.accept();
+				System.out.println("\nNew client accepted.\n");
+				ClientHandler handler = new ClientHandler(client);
+				clientList.insertFirst(handler);
+				Thread thread = new Thread(handler);
+				thread.start();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} while(running);
+		System.out.println("\nServer shutdown!\n");
 	}
 	
 }
